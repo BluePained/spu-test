c7c3ff3c498cc21c584c00f09be699e3f98a5f2d
BluePained <inwisetjiro666@gmail.com>
2024-02-17 15:26:17 +0700

    BuilderWalk

--- Assets/Script/Buildings/Building.cs
+++ Assets/Script/Buildings/Building.cs
@@ -26,7 +26,10 @@ public class Building : Structure
     public bool IsHQ { get { return isHQ; } }
     [SerializeField] private float intoTheGround = 5f;
     public float IntoTheGround { get { return intoTheGround; } }
-
+    private float timer = 0f; //Constructing timer
+    public float Timer { get { return timer; } set { timer = value; } }
+    private float waitTime = 0.5f; //How fast it will be construct, higher is longer
+    public float WaitTime { get { return waitTime; } set { waitTime = value; } }
     // Start is called before the first frame update
     void Start()
     {
